ebf414d938cf23bc633e152a48775879ff1025c2
Habibi Alfahrezi <habibialfahrezi6@gmail.com>
2024-09-23 10:29:04 +0700

    Start Project

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+        {/* Accordian Component */}
+        <p>Accordian</p>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/accordians/data.js
@@ -0,0 +1,24 @@
+[
+    {
+      "id": 1,
+      "question": "What is the capital of France?",
+      "answer": "Paris is the capital city of France, known for its rich history, culture, and iconic landmarks like the Eiffel Tower and the Louvre Museum. It is also considered a global center for art, fashion, and gastronomy."
+    },
+    {
+      "id": 2,
+      "question": "Who wrote 'Romeo and Juliet'?",
+      "answer": "'Romeo and Juliet' is a famous play written by William Shakespeare in the late 16th century. It tells the tragic love story of two young star-crossed lovers whose deaths ultimately reconcile their feuding families. The play has been adapted into various formats, including films, musicals, and ballets."
+    },
+    {
+      "id": 3,
+      "question": "What is the largest planet in our solar system?",
+      "answer": "Jupiter is the largest planet in our solar system, more than twice as massive as all the other planets combined. It is a gas giant, primarily composed of hydrogen and helium, with a Great Red Spot, a massive storm that has been raging for centuries. Jupiter also has at least 79 moons, the largest being Ganymede, which is even bigger than Mercury."
+    },
+    {
+      "id": 4,
+      "question": "In what year did the Titanic sink?",
+      "answer": "The RMS Titanic sank in the early hours of April 15, 1912, after hitting an iceberg during its maiden voyage from Southampton to New York City. Despite being deemed 'unsinkable', the ship carried only enough lifeboats for about half of its passengers, leading to the deaths of over 1,500 people in one of the deadliest maritime disasters in history."
+    }
+  ]
+
+export default data;
